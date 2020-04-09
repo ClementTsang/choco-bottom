432dd042640e4f523fe2fc64da445448eaab9884
ClementTsang <cjhtsang@uwaterloo.ca>
2020-04-08 21:24:32 -0400

    0.3.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/ClementTsang/bottom/releases/download/0.2.2/bottom_i686-pc-windows-msvc.zip'
-$url64      = 'https://github.com/ClementTsang/bottom/releases/download/0.2.2/bottom_x86_64-pc-windows-msvc.zip'
+$url        = 'https://github.com/ClementTsang/bottom/releases/download/0.3.0/bottom_i686-pc-windows-msvc.zip'
+$url64      = 'https://github.com/ClementTsang/bottom/releases/download/0.3.0/bottom_x86_64-pc-windows-msvc.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,9 +11,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
 
-  checksum      = 'CADCA981FB45D23E0B03C214E2443D853F7E5B7E'
+  checksum      = '53E3BBF0F50643B5B798769CB59291FA090A6704'
   checksumType  = 'sha1'
-  checksum64    = '5DBBF590660633549FDC655554FDD32C3C8BF937'
+  checksum64    = '295F906814BA90A62FC40F5024B05E113863D2FB'
   checksumType64= 'sha1'
 
 }
