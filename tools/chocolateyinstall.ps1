f49724f506338a51bd5601179974cb682081afd4
ClementTsang <ClementTsang@users.noreply.github.com>
2021-09-12 20:45:57 +0000

    Actions: Update choco package files to 0.6.4

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/ClementTsang/bottom/releases/download/0.6.3/bottom_i686-pc-windows-msvc.zip'
-$url64      = 'https://github.com/ClementTsang/bottom/releases/download/0.6.3/bottom_x86_64-pc-windows-msvc.zip'
+$url        = 'https://github.com/ClementTsang/bottom/releases/download/0.6.4/bottom_i686-pc-windows-msvc.zip'
+$url64      = 'https://github.com/ClementTsang/bottom/releases/download/0.6.4/bottom_x86_64-pc-windows-msvc.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,9 +11,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
 
-  checksum      = 'c75735cb17cc5cc869147513a424a9e9ec7296df'
+  checksum      = '57fc3a8c5811b6ef62834ae308b1d31a784c6953'
   checksumType  = 'sha1'
-  checksum64    = 'f7d2d0585143619e91cde3bc5cc87caa2c751c27'
+  checksum64    = '978fa1b5f435823ab6577c6ff43c4e059ab5b170'
   checksumType64= 'sha1'
 
 }
