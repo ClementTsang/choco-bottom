347405213395332746f82e24a49718a4f086f56a
ClementTsang <cjhtsang@uwaterloo.ca>
2022-02-02 22:20:54 -0800

    disable 32-bit

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,6 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/ClementTsang/bottom/releases/download/0.6.8/bottom_i686-pc-windows-msvc.zip'
-$url64      = 'https://github.com/ClementTsang/bottom/releases/download/0.6.8/bottom_x86_64-pc-windows-msvc.zip'
+$url      = 'https://github.com/ClementTsang/bottom/releases/download/0.6.8/bottom_x86_64-pc-windows-msvc.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,12 +8,8 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-  url64bit      = $url64
 
-  checksum      = '32cb392b23406a5fcfd2ae2bc9e16557b84b7742'
+  checksum      = '4e577fa820e49ff6a35b55e0f8f9ff08eae6479c'
   checksumType  = 'sha1'
-  checksum64    = '4e577fa820e49ff6a35b55e0f8f9ff08eae6479c'
-  checksumType64= 'sha1'
-
 }
 Install-ChocolateyZipPackage @packageArgs
