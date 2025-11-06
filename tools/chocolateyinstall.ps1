fd3d7ed109c41fcb89d40e16dfb97bb90125f951
ClementTsang <ClementTsang@users.noreply.github.com>
2025-11-06 15:49:52 +0000

    Actions: Update choco package files to 0.11.3

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/ClementTsang/bottom/releases/download/0.11.2/bottom_x86_64-pc-windows-msvc.zip'
+$url        = 'https://github.com/ClementTsang/bottom/releases/download/0.11.3/bottom_x86_64-pc-windows-msvc.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-  checksum      = '240dd74f44be0f2e0ca3061fed052bd958193dc2'
+  checksum      = 'c7cb4efbffe4eb28fc055f7638977d71fb567120'
   checksumType  = 'sha1'
 
 }
